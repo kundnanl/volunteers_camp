d0a8c474edb6f7d3a28ad660b7cd4a88c3ae4d46
kundnanl <kundnanl@sheridancollege.ca>
2023-08-22 23:56:20 -0400

    Integrated Organization Events

--- private/organization_dashboard.php
+++ private/organization_dashboard.php
@@ -6,48 +6,83 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'organization') {
     exit();
 }
 
-// Replace the example arrays with actual data fetched from the database
-$hostedEvents = array(
-    array('name' => 'Charity Fundraiser', 'date' => '2023-08-15', 'location' => 'Community Hall'),
-    array('name' => 'Blood Donation Camp', 'date' => '2023-08-25', 'location' => 'Hospital')
-);
+$host = "localhost";
+$username = "root";
+$password = "mysql";
+$database = "php";
 
-$volunteerRegistrations = array(
-    array('volunteer' => 'John Doe', 'event' => 'Charity Fundraiser'),
-    array('volunteer' => 'Jane Smith', 'event' => 'Blood Donation Camp')
-);
+$conn = mysqli_connect($host, $username, $password, $database);
 
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+$user_id = $_SESSION['user_id'];
+
+// Fetch hosted events
+$hostedEventsQuery = "SELECT * FROM events WHERE organization_id = $user_id";
+$hostedEventsResult = mysqli_query($conn, $hostedEventsQuery);
+
+// Handle event creation form submission
+$eventCreationMessage = "";
+if (isset($_POST['create_event'])) {
+    $event_name = mysqli_real_escape_string($conn, $_POST['event_name']);
+    $event_date = mysqli_real_escape_string($conn, $_POST['event_date']);
+    $event_location = mysqli_real_escape_string($conn, $_POST['event_location']);
+    $volunteers_needed = mysqli_real_escape_string($conn, $_POST['volunteers_needed']);
+
+    $insert_query = "INSERT INTO events (organization_id, event_name, event_date, event_location, volunteers_needed)
+                     VALUES ($user_id, '$event_name', '$event_date', '$event_location', $volunteers_needed)";
+
+    if (mysqli_query($conn, $insert_query)) {
+        $eventCreationMessage = "Event successfully created.";
+    } else {
+        $eventCreationMessage = "Event creation failed.";
+    }
+}
+
+mysqli_close($conn);
 ?>
 
-<?php include('header.php'); ?>
-<main>
-    <h1>Welcome, Organization!</h1>
-    <h2>Hosted Events</h2>
-    <?php if (empty($hostedEvents)) : ?>
-        <p>No hosted events found.</p>
-    <?php else : ?>
-        <ul>
-            <?php foreach ($hostedEvents as $event) : ?>
-                <li>
-                    <strong><?php echo $event['name']; ?></strong>
-                    <br>Date: <?php echo $event['date']; ?>
-                    <br>Location: <?php echo $event['location']; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Organization Dashboard</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <main>
+        <h1>Welcome, Organization!</h1>
+        
+        <h2>Create Event</h2>
+        <form method="post" action="organization_dashboard.php">
+            <label>Event Name: <input type="text" name="event_name" required></label><br>
+            <label>Event Date: <input type="date" name="event_date" required></label><br>
+            <label>Event Location: <input type="text" name="event_location" required></label><br>
+            <label>Volunteers Needed: <input type="number" name="volunteers_needed" required></label><br>
+            <input type="submit" name="create_event" value="Create Event">
+        </form>
+        
+        <p><?php echo $eventCreationMessage; ?></p>
 
-    <h2>Volunteer Registrations</h2>
-    <?php if (empty($volunteerRegistrations)) : ?>
-        <p>No volunteer registrations found.</p>
-    <?php else : ?>
-        <ul>
-            <?php foreach ($volunteerRegistrations as $registration) : ?>
-                <li>
-                    <strong><?php echo $registration['volunteer']; ?></strong> - <?php echo $registration['event']; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-</main>
-<?php include('footer.php'); ?>
+        <h2>Hosted Events</h2>
+        <?php if (mysqli_num_rows($hostedEventsResult) === 0) : ?>
+            <p>No hosted events found.</p>
+        <?php else : ?>
+            <ul>
+                <?php while ($event = mysqli_fetch_assoc($hostedEventsResult)) : ?>
+                    <li>
+                        <strong><?php echo $event['event_name']; ?></strong>
+                        <br>Date: <?php echo $event['event_date']; ?>
+                        <br>Location: <?php echo $event['event_location']; ?>
+                        <br>Volunteers Needed: <?php echo $event['volunteers_needed']; ?>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+        <?php endif; ?>
+    </main>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
